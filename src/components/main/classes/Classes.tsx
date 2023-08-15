3b743bdcbb2d5ea4bca6464adf79937bcba35e80
Mussorable <redduck5601@gmail.com>
2023-08-15 16:14:17 +0200

    fixed classes images

--- src/components/main/classes/Classes.tsx
+++ src/components/main/classes/Classes.tsx
@@ -2,96 +2,44 @@ import "./classes.scss";
 
 import { createParallax } from "../../../utils/effects";
 
-import { default as barbarian } from "../../../assets/classes/barbarian.svg";
-import { default as rogue } from "../../../assets/classes/rogue.svg";
-import { default as sorcerer } from "../../../assets/classes/sorcerer.svg";
-import { default as necromancer } from "../../../assets/classes/necromancer.svg";
-import { default as druid } from "../../../assets/classes/druid.svg";
-
-import { default as barbarianHero } from "../../../assets/classes/barbarian-prlx.png";
-import { default as rogueHero } from "../../../assets/classes/rogue-prlx.png";
-import { default as sorcererHero } from "../../../assets/classes/sorcerer-prlx.png";
-import { default as necromancerHero } from "../../../assets/classes/necromancer-prlx.png";
-import { default as druidHero } from "../../../assets/classes/druid-prlx.png";
-
 import React, { useEffect, useState } from "react";
 
 interface HeroClass {
   title: string;
-  imageURL: string;
-  imageURLHero: string;
+  smallIcon: string;
+  heroImage: string;
   description: string;
 }
-
-interface Skill {
-  title: string;
-  description: string;
-  imageURL: string;
-}
-
-const classes: Array<HeroClass> = [
-  {
-    title: "Barbarian",
-    imageURL: barbarian,
-    imageURLHero: barbarianHero,
-    description:
-      "The Barbarian has unparalleled strength and expertly wields an entire arsenal in battle, with a weapon for every occasion. He bellows intimidating war cries and unleashes ground-shaking slams to send approaching hordes reeling.",
-  },
-  {
-    title: "Rogue",
-    imageURL: rogue,
-    imageURLHero: rogueHero,
-    description:
-      "The Rogue is an adaptable, agile warrior who can specialize in ranged or close quarters combat. She can best any foe with her imbued weapons, perform powerful combo attacks, and can augment her arsenal with deadly poisons and shadow magic to slay demons with impunity.",
-  },
-  {
-    title: "Sorcerer",
-    imageURL: sorcerer,
-    imageURLHero: sorcererHero,
-    description:
-      "The Sorcerer shapes the elements into whatever form is necessary to ensure victory, from hurling bolts of lightning, impaling her foes upon jagged spikes of ice, and raining flaming meteors down from the sky.",
-  },
-  {
-    title: "Necromancer",
-    imageURL: necromancer,
-    imageURLHero: necromancerHero,
-    description:
-      "Necromancers are cunning summoners that conjure vengeful hordes of the undead. Their Essence flows into three powerful bastions of Bone, Blood, or Shadow to bring low their enemies.",
-  },
-  {
-    title: "Druid",
-    imageURL: druid,
-    imageURLHero: druidHero,
-    description:
-      "The Druid is a savage shapeshifter, fluidly transforming between the forms of a towering bear or a vicious werewolf to fight alongside the creatures of the wild. He also commands the power of earth, wind, and storm, unleashing nature’s wrath to devastating effect.",
-  },
-];
 
 const Classes = () => {
-  const [currentClass, setCurrentClass] = useState<HeroClass>(classes[0]);
-  const [classSkills, setClassSkills] = useState<any>();
+  const [heroClasses, setHeroClasses] = useState<HeroClass[]>();
+  const [currentClass, setCurrentClass] = useState<HeroClass>();
 
   useEffect(() => {
-    const fetchSkills = async () => {
-      const response = await fetch("http://localhost:8080/skills");
-      const skills = await response.json();
+    const getClasses = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/classes/get-all");
+        const classes = await response.json();
 
-      console.log(skills);
-
-      setClassSkills(skills);
+        setHeroClasses(Object.values(classes));
+      } catch (error) {
+        throw error;
+      }
     };
 
-    fetchSkills();
+    getClasses();
   }, []);
 
   const handleClassClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const currentClass = classes.find(
-      (hero) => hero.title === e.currentTarget.name
-    );
-    if (!currentClass) {
-      return console.error("Try to reload page. Undefined class name.");
+    if (heroClasses) {
+      const currentClass = heroClasses.find(
+        (hero) => hero.title === e.currentTarget.name
+      );
+      if (!currentClass) {
+        return console.error("Try to reload page. Undefined class name.");
+      }
+      setCurrentClass(currentClass);
     }
-    setCurrentClass(currentClass);
   };
 
   return (
@@ -100,8 +48,8 @@ const Classes = () => {
         <div className="class-list-wrapper">
           <div className="class-list-selector">
             <ul className="class-list">
-              {classes &&
-                classes.map((hero) => {
+              {heroClasses &&
+                heroClasses.map((hero) => {
                   return (
                     <li
                       className="class-list-item"
@@ -114,7 +62,7 @@ const Classes = () => {
                       >
                         <img
                           className="class-logo"
-                          src={hero.imageURL}
+                          src={hero.smallIcon}
                           alt="class logo"
                           width="50"
                           height="50"
@@ -146,17 +94,11 @@ const Classes = () => {
               <img
                 height="500"
                 className="class-parallax parallax-enabled"
-                src={currentClass.imageURLHero}
+                src={currentClass.heroImage}
                 alt="hero class"
               />
             )}
           </div>
-        </div>
-        <div className="class-skills-wrapper">
-          {classSkills &&
-            classSkills.map((skill: Skill) => (
-              <img key={skill.title} src={skill.imageURL} />
-            ))}
         </div>
       </div>
     </main>
